958faeac0666ea657b145f5d3b658a697b374a53
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2007-08-08 14:40:28 +0000

    Added unit test

---
+++ test/MessageDigestHelperTest.cc
@@ -0,0 +1,50 @@
+#include "MessageDigestHelper.h"
+#include "Util.h"
+#include "DefaultDiskWriter.h"
+#include <cppunit/extensions/HelperMacros.h>
+
+using namespace std;
+
+class MessageDigestHelperTest:public CppUnit::TestFixture {
+
+  CPPUNIT_TEST_SUITE(MessageDigestHelperTest);
+  CPPUNIT_TEST(testDigestDiskWriter);
+  CPPUNIT_TEST(testDigestFilename);
+  CPPUNIT_TEST(testDigestData);
+  CPPUNIT_TEST_SUITE_END();
+private:
+
+public:
+  void setUp() {
+  }
+
+  void testDigestDiskWriter();
+  void testDigestFilename();
+  void testDigestData();
+};
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION( MessageDigestHelperTest );
+
+void MessageDigestHelperTest::testDigestDiskWriter() {
+  DiskWriterHandle diskio = new DefaultDiskWriter();
+  diskio->openExistingFile("4096chunk.txt");
+  CPPUNIT_ASSERT_EQUAL(string("608cabc0f2fa18c260cafd974516865c772363d5"),
+		       MessageDigestHelper::digest("sha1", diskio));
+
+  CPPUNIT_ASSERT_EQUAL(string("7a4a9ae537ebbbb826b1060e704490ad0f365ead"),
+		       MessageDigestHelper::digest("sha1", diskio, 5, 100));
+}
+
+void MessageDigestHelperTest::testDigestFilename()
+{
+  CPPUNIT_ASSERT_EQUAL(string("608cabc0f2fa18c260cafd974516865c772363d5"),
+		       MessageDigestHelper::digest("sha1", "4096chunk.txt"));
+}
+
+void MessageDigestHelperTest::testDigestData()
+{
+  string data = "aria2";
+  CPPUNIT_ASSERT_EQUAL(string("f36003f22b462ffa184390533c500d8989e9f681"),
+		       MessageDigestHelper::digest("sha1", data.c_str(), data.size()));
+}
