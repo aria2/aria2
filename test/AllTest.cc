4b67290f0098b04f88ccbb86c801fec4ab57c047
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2008-02-21 02:21:14 +0000

    2008-02-21  Tatsuhiro Tsujikawa  <tujikawa at rednoah dot com>
    	* test/AllTest.cc: Added OpenSSL, GNUTLS initialization code.

--- test/AllTest.cc
+++ test/AllTest.cc
@@ -2,6 +2,13 @@
 #include <cppunit/CompilerOutputter.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
+#ifdef HAVE_LIBSSL
+# include <openssl/err.h>
+# include <openssl/ssl.h>
+#endif // HAVE_LIBSSL
+#ifdef HAVE_LIBGNUTLS
+# include <gnutls/gnutls.h>
+#endif // HAVE_LIBGNUTLS
 
 using aria2::SharedHandle;
 using aria2::SingletonHolder;
@@ -14,11 +21,25 @@ int main(int argc, char* argv[]) {
   runner.setOutputter(new CppUnit::CompilerOutputter(&runner.result(), std::cerr));
   
   // setup
+
+#ifdef HAVE_LIBSSL
+  // for SSL initialization
+  SSL_load_error_strings();
+  SSL_library_init();
+#endif // HAVE_LIBSSL
+#ifdef HAVE_LIBGNUTLS
+  gnutls_global_init();
+#endif // HAVE_LIBGNUTLS
+
   SharedHandle<aria2::CookieBoxFactory> cookieBoxFactory = new aria2::CookieBoxFactory();
   SingletonHolder<SharedHandle<aria2::CookieBoxFactory> >::instance(cookieBoxFactory);
 
   // Run the tests.
   bool successfull = runner.run();
+
+#ifdef HAVE_LIBGNUTLS
+  gnutls_global_deinit();
+#endif // HAVE_LIBGNUTLS
 
   return successfull ? 0 : 1;
 }
