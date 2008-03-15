83b746d94cce789c93aef8370b387715ddf9b776
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2008-03-15 03:06:07 +0000

    2008-03-15  Tatsuhiro Tsujikawa  <tujikawa at rednoah dot com>
    	Suppress content-range error when 'Content-Range' response header
    	doesn't contains bytes-unit specifier 'bytes'
    	* src/HttpHeader.cc (getRange)
    	* test/HttpHeaderTest.cc

--- test/HttpHeaderTest.cc
+++ test/HttpHeaderTest.cc
@@ -20,14 +20,27 @@ CPPUNIT_TEST_SUITE_REGISTRATION( HttpHeaderTest );
 
 void HttpHeaderTest::testGetRange()
 {
-  HttpHeader httpHeader;
-  httpHeader.put("Content-Range", "bytes 1-499/1234");
-
-  SharedHandle<Range> range = httpHeader.getRange();
-
-  CPPUNIT_ASSERT_EQUAL((int64_t)1, range->getStartByte());
-  CPPUNIT_ASSERT_EQUAL((int64_t)499, range->getEndByte());
-  CPPUNIT_ASSERT_EQUAL((int64_t)1234, range->getEntityLength());
+  {
+    HttpHeader httpHeader;
+    httpHeader.put("Content-Range", "bytes 1-499/1234");
+    
+    SharedHandle<Range> range = httpHeader.getRange();
+    
+    CPPUNIT_ASSERT_EQUAL((int64_t)1, range->getStartByte());
+    CPPUNIT_ASSERT_EQUAL((int64_t)499, range->getEndByte());
+    CPPUNIT_ASSERT_EQUAL((int64_t)1234, range->getEntityLength());
+  }
+  {
+    HttpHeader httpHeader;
+    httpHeader.put("Content-Range",
+		   "9223372036854775800-9223372036854775801/9223372036854775807");
+    
+    SharedHandle<Range> range = httpHeader.getRange();
+    
+    CPPUNIT_ASSERT_EQUAL(9223372036854775800LL, range->getStartByte());
+    CPPUNIT_ASSERT_EQUAL(9223372036854775801LL, range->getEndByte());
+    CPPUNIT_ASSERT_EQUAL(9223372036854775807LL, range->getEntityLength());
+  }
 }
 
 } // namespace aria2
