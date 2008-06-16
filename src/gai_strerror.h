4da1360727423c2d6fe9e573abebebfa92e2dcb9
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2008-06-16 12:07:21 +0000

    2008-06-16  Tatsuhiro Tsujikawa  <tujikawa at rednoah dot com>
    	Applied Ross's aria2-0.13.2+1-mingw-4.patch. In this commit,
    	only gai_strerror.h is modified.
    	* src/gai_strerror.h: Fixed gai_strerror() function to report 
    all
    	winsock errors in MinGW.

--- src/gai_strerror.h
+++ src/gai_strerror.h
@@ -41,96 +41,6 @@ extern "C" {
 # include "config.h"
 #endif // HAVE_CONFIG_H
 
-/********************************************************************/
-/*
- * Undefine all the macros.
- * <netdb.h> might defines some of them.
- */
-#ifdef EAI_ADDRFAMILY
-#undef EAI_ADDRFAMILY
-#endif
-#ifdef EAI_AGAIN
-#undef EAI_AGAIN
-#endif
-#ifdef EAI_BADFLAGS
-#undef EAI_BADFLAGS
-#endif
-#ifdef EAI_FAIL
-#undef EAI_FAIL
-#endif
-#ifdef EAI_FAMILY
-#undef EAI_FAMILY
-#endif
-#ifdef EAI_MEMORY
-#undef EAI_MEMORY
-#endif
-#ifdef EAI_NONAME
-#undef EAI_NONAME
-#endif
-#ifdef EAI_OVERFLOW
-#undef EAI_OVERFLOW
-#endif
-#ifdef EAI_SERVICE
-#undef EAI_SERVICE
-#endif
-#ifdef EAI_SOCKTYPE
-#undef EAI_SOCKTYPE
-#endif
-#ifdef EAI_SYSTEM
-#undef EAI_SYSTEM
-#endif
-
-#ifdef AI_PASSIVE
-#undef AI_PASSIVE
-#endif
-#ifdef AI_CANONNAME
-#undef AI_CANONNAME
-#endif
-#ifdef AI_NUMERICHOST
-#undef AI_NUMERICHOST
-#endif
-#ifdef AI_NUMERICSERV
-#undef AI_NUMERICSERV
-#endif
-#ifdef AI_V4MAPPED
-#undef AI_V4MAPPED
-#endif
-#ifdef AI_ALL
-#undef AI_ALL
-#endif
-#ifdef AI_ADDRCONFIG
-#undef AI_ADDRCONFIG
-#endif
-#ifdef AI_DEFAULT
-#undef AI_DEFAULT
-#endif
-
-#ifdef NI_NOFQDN
-#undef NI_NOFQDN
-#endif
-#ifdef NI_NUMERICHOST
-#undef NI_NUMERICHOST
-#endif
-#ifdef NI_NAMEREQD
-#undef NI_NAMEREQD
-#endif
-#ifdef NI_NUMERICSERV
-#undef NI_NUMERICSERV
-#endif
-#ifdef NI_NUMERICSCOPE
-#undef NI_NUMERICSCOPE
-#endif
-
-#ifdef NI_DGRAM
-#undef NI_DGRAM
-#endif
-#ifdef NI_MAXHOST
-#undef NI_MAXHOST
-#endif
-#ifdef NI_MAXSERV
-#undef NI_MAXSERV
-#endif
-
 /*
  * Fake struct and function names.
  * <netdb.h> might declares all or some of them.
@@ -138,22 +48,6 @@ extern "C" {
 #if defined(HAVE_GAI_STRERROR)
 #define gai_strerror my_gai_strerror
 #endif
-
-/********************************************************************/
-/*
- * Error codes.
- */
-#define EAI_ADDRFAMILY	1
-#define EAI_AGAIN	2
-#define EAI_BADFLAGS	3
-#define EAI_FAIL	4
-#define EAI_FAMILY	5
-#define EAI_MEMORY	6
-#define EAI_NONAME	7
-#define EAI_OVERFLOW	8
-#define EAI_SERVICE	9
-#define EAI_SOCKTYPE	10
-#define EAI_SYSTEM	11
 
 /*
  * Functions.
