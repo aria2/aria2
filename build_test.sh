9dda6c4b6915020aaa4cef76b2cd6f83eca3fb76
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2008-06-20 13:34:53 +0000

    2008-06-20  Tatsuhiro Tsujikawa  <tujikawa at rednoah dot com>
    	Added build test script
    	* build_test.sh

---
+++ build_test.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+exec 2>&1
+
+BUILD_TEST_DIR=/tmp/aria2_build_test
+LOG=build_test.log
+
+if [ ! -d $BUILD_TEST_DIR ]; then
+    mkdir $BUILD_TEST_DIR || echo "Failed to create directory $BUILD_TEST_DIR" \
+	&& exit -1
+fi
+
+echo -n "Starting build test "
+echo `date`
+
+# build CONFIGURE_OPTS BIN_SUFFIX DESC
+build()
+{
+    echo -n "new build() started at "
+    echo `date`
+    echo "*** configure opts=$1"
+    BIN_NAME=aria2c_$2
+    if [ -f $BUILD_TEST_DIR/$BIN_NAME ]; then
+	echo "$BIN_NAME exists, skipping"
+	return
+    fi
+    ./configure $1 && make -j2 check && \
+	cp src/aria2c $BUILD_TEST_DIR/aria2c_$2 && \
+	cp config.log $BUILD_TEST_DIR/config.log_$2
+}
+
+clear()
+{
+    for file in `ls $BUILD_TEST_DIR`; do
+	rm -f $BUILD_TEST_DIR/$file;
+    done
+}
+
+case "$1" in
+    clear)
+	clear
+	;;
+    *)
+	build "--without-gnutls" "openssl"
+	build "--without-gnutls --without-openssl" "nossl"
+	build "--without-libcares" "nocares"
+	build "--without-libxml2" "nolibxml2"
+	build "--without-libxml2 --without-libexpat" "noxml"
+	# Feature combinations
+	build "--disable-bittorrent" "nobt"
+	build "--disable-metalink" "noml"
+	build "--disable-bittorrent --disable-metalink" "nobt_noml"
+	build "--disable-epoll" "noepoll"
+	build "--disable-epoll --without-libcares" "noepoll_nocares"
+	;;
+esac
