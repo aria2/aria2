bf1f56481deb75b8cc07f3720fea53bcb8774f8d
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2008-07-22 16:10:31 +0000

    2008-07-23  Tatsuhiro Tsujikawa  <tujikawa at rednoah dot com>
    	Added a test for libz. Quoted where it is necessary.
    	* build_test.sh

--- build_test.sh
+++ build_test.sh
@@ -3,10 +3,10 @@
 exec 2>&1
 
 BUILD_TEST_DIR=/tmp/aria2_build_test
-LOG=build_test.log
 
-if [ ! -d $BUILD_TEST_DIR ]; then
-    mkdir $BUILD_TEST_DIR || echo "Failed to create directory $BUILD_TEST_DIR" \
+if [ ! -d "$BUILD_TEST_DIR" ]; then
+    mkdir "$BUILD_TEST_DIR" \
+	|| echo "Failed to create directory $BUILD_TEST_DIR" \
 	&& exit -1
 fi
 
@@ -19,20 +19,21 @@ build()
     echo -n "new build() started at "
     echo `date`
     echo "*** configure opts=$1"
-    BIN_NAME=aria2c_$2
-    if [ -f $BUILD_TEST_DIR/$BIN_NAME ]; then
+    BIN_NAME="aria2c_$2"
+    if [ -f "$BUILD_TEST_DIR/$BIN_NAME" ]; then
 	echo "$BIN_NAME exists, skipping"
 	return
     fi
-    ./configure $1 && make -j2 check && \
-	cp src/aria2c $BUILD_TEST_DIR/aria2c_$2 && \
-	cp config.log $BUILD_TEST_DIR/config.log_$2
+    ./configure $1 \
+	&& cp config.log "$BUILD_TEST_DIR/config.log_$2" \
+	&& LANG=C make -j2 check > "$BUILD_TEST_DIR/aria2c_$2.log" \
+	&& cp src/aria2c "$BUILD_TEST_DIR/aria2c_$2"
 }
 
 clear()
 {
     for file in `ls $BUILD_TEST_DIR`; do
-	rm -f $BUILD_TEST_DIR/$file;
+	rm -f "$BUILD_TEST_DIR/$file";
     done
 }
 
@@ -41,11 +42,13 @@ case "$1" in
 	clear
 	;;
     *)
+	# Library combinations
 	build "--without-gnutls" "openssl"
 	build "--without-gnutls --without-openssl" "nossl"
 	build "--without-libcares" "nocares"
 	build "--without-libxml2" "nolibxml2"
 	build "--without-libxml2 --without-libexpat" "noxml"
+	build "--without-libz" "nozlib"
 	# Feature combinations
 	build "--disable-bittorrent" "nobt"
 	build "--disable-metalink" "noml"
