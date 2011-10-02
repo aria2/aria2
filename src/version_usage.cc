8938cef9b3591da8a1f10d6f4915da11e077c973
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2011-10-02 16:14:03 +0900

    Made several messages translatable

--- src/version_usage.cc
+++ src/version_usage.cc
@@ -58,27 +58,26 @@ void showVersion() {
             << "Copyright (C) 2006, 2011 Tatsuhiro Tsujikawa" << "\n"
             << "\n"
             <<
-    "This program is free software; you can redistribute it and/or modify\n"
-    "it under the terms of the GNU General Public License as published by\n"
-    "the Free Software Foundation; either version 2 of the License, or\n"
-    "(at your option) any later version.\n"
+    _("This program is free software; you can redistribute it and/or modify\n"
+      "it under the terms of the GNU General Public License as published by\n"
+      "the Free Software Foundation; either version 2 of the License, or\n"
+      "(at your option) any later version.\n"
+      "\n"
+      "This program is distributed in the hope that it will be useful,\n"
+      "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+      "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+      "GNU General Public License for more details.\n")
             << "\n"
-            <<
-    "This program is distributed in the hope that it will be useful,\n"
-    "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
-    "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
-    "GNU General Public License for more details.\n"
-            << "\n"
-            << "** Configuration **" << "\n"
-            << "Enabled Features: "
+            << _("** Configuration **") << "\n"
+            << _("Enabled Features") << ": "
             << FeatureConfig::getInstance()->featureSummary() << "\n"
 #ifdef ENABLE_MESSAGE_DIGEST
-            << "Hash Algorithms: "
+            << _("Hash Algorithms") << ": "
             << MessageDigest::getSupportedHashTypeString() << "\n"
 #endif // ENABLE_MESSAGE_DIGEST
             << "\n"
             << fmt(_("Report bugs to %s"), PACKAGE_BUGREPORT) << "\n"
-            << "Visit " << PACKAGE_URL << std::endl;
+            << _("Visit") << " " << PACKAGE_URL << std::endl;
 }
 
 void showUsage(const std::string& keyword, const OptionParser& oparser) {
