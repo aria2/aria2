988878930e50dcca7ac3a3d63cb0410080c6dbd3
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2013-01-27 17:32:06 +0900

    Check SSL_OP_NO_COMPRESSION is available before using it

--- src/LibsslTLSContext.cc
+++ src/LibsslTLSContext.cc
@@ -57,8 +57,11 @@ TLSContext::TLSContext(TLSSessionSide side)
                      ERR_error_string(ERR_get_error(), 0)));
   }
   // Disable SSLv2 and enable all workarounds for buggy servers
-  SSL_CTX_set_options(sslCtx_, SSL_OP_ALL|SSL_OP_NO_SSLv2|
-                      SSL_OP_NO_COMPRESSION);
+  SSL_CTX_set_options(sslCtx_, SSL_OP_ALL | SSL_OP_NO_SSLv2
+#ifdef SSL_OP_NO_COMPRESSION
+                      | SSL_OP_NO_COMPRESSION
+#endif // SSL_OP_NO_COMPRESSION
+                      );
   SSL_CTX_set_mode(sslCtx_, SSL_MODE_AUTO_RETRY);
   SSL_CTX_set_mode(sslCtx_, SSL_MODE_ENABLE_PARTIAL_WRITE);
   #ifdef SSL_MODE_RELEASE_BUFFERS
