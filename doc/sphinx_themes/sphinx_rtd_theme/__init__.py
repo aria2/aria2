4654f1974a5faa832518ba3875ecb3e77224e656
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2015-05-07 21:22:56 +0900

    Update sphinx_rtd_theme

--- doc/sphinx_themes/sphinx_rtd_theme/__init__.py
+++ doc/sphinx_themes/sphinx_rtd_theme/__init__.py
@@ -5,7 +5,7 @@ From https://github.com/ryan-roemer/sphinx-bootstrap-theme.
 """
 import os
 
-VERSION = (0, 1, 5)
+VERSION = (0, 1, 8)
 
 __version__ = ".".join(str(v) for v in VERSION)
 __version_full__ = __version__
