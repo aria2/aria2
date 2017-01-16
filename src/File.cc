eecb5e055e5e2659d7da8d33d8acf83efac63a94
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2017-01-16 21:33:04 +0900

    clang-format

--- src/File.cc
+++ src/File.cc
@@ -79,7 +79,7 @@ bool File::exists()
   return fillStat(fstat) == 0;
 }
 
-bool File::exists(std::string &err)
+bool File::exists(std::string& err)
 {
   a2_struct_stat fstat;
   if (fillStat(fstat) != 0) {
