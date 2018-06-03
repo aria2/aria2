376854cfc4dd2141eeb1358fca0a2df84b3d1955
Ben Chaney <chaneybenjamini@gmail.com>
2018-06-03 01:10:36 -0400

    Fix openssl API version logic for libressl 2.7.x

--- src/libssl_compat.h
+++ src/libssl_compat.h
@@ -44,6 +44,7 @@
 #endif // !defined(LIBRESSL_VERSION_NUMBER)
 
 #define OPENSSL_101_API                                                        \
-  (!LIBRESSL_IN_USE && OPENSSL_VERSION_NUMBER >= 0x1010000fL)
+  ((!LIBRESSL_IN_USE && OPENSSL_VERSION_NUMBER >= 0x1010000fL) || \
+  (LIBRESSL_IN_USE && LIBRESSL_VERSION_NUMBER >= 0x20700000L))
 
 #endif // LIBSSL_COMPAT_H
