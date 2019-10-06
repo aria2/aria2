53b3169a22fd60f07ea0f1ef5b284cb6944b5d31
Nils Maier <maierman@web.de>
2019-10-06 13:36:08 +0200

    AppleTLS: even more correctly define kTLSProtocol13
    Yes, again

--- src/AppleTLSSession.cc
+++ src/AppleTLSSession.cc
@@ -53,12 +53,12 @@
 
 namespace {
 #if !defined(__MAC_10_8)
-static const SSLProtocol kTLSProtocol11 = 7;
-static const SSLProtocol kTLSProtocol12 = 8;
+static const SSLProtocol kTLSProtocol11 = (SSLProtocol)7;
+static const SSLProtocol kTLSProtocol12 = (SSLProtocol)8;
 #endif
 
 #if !defined(__MAC_10_13)
-static const SSLProtocol kTLSProtocol13 = 10;
+static const SSLProtocol kTLSProtocol13 = (SSLProtocol)10;
 #endif
 
 #ifndef CIPHER_NO_DHPARAM
