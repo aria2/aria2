ba3396f7bb210b53fb940f274a0622ad9799545c
Tatsuhiro Tsujikawa <tatsuhiro.t@gmail.com>
2022-04-09 15:48:16 +0900

    Better entropy for getRandomBytes

---
+++ test/SimpleRandomizerTest.cc
@@ -0,0 +1,70 @@
+/* <!-- copyright */
+/*
+ * aria2 - The high speed download utility
+ *
+ * Copyright (C) 2022 Tatsuhiro Tsujikawa
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library under certain conditions as described in each
+ * individual source file, and distribute linked combinations
+ * including the two.
+ * You must obey the GNU General Public License in all respects
+ * for all of the code used other than OpenSSL.  If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so.  If you
+ * do not wish to do so, delete this exception statement from your
+ * version.  If you delete this exception statement from all source
+ * files in the program, then also delete it here.
+ */
+/* copyright --> */
+#include "SimpleRandomizer.h"
+
+#include <set>
+#include <array>
+
+#include <cppunit/extensions/HelperMacros.h>
+
+namespace aria2 {
+
+class SimpleRandomizerTest : public CppUnit::TestFixture {
+  CPPUNIT_TEST_SUITE(SimpleRandomizerTest);
+  CPPUNIT_TEST(testGetRandomBytes);
+  CPPUNIT_TEST_SUITE_END();
+
+public:
+  void testGetRandomBytes();
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(SimpleRandomizerTest);
+
+void SimpleRandomizerTest::testGetRandomBytes()
+{
+  std::set<std::string> set;
+
+  constexpr size_t n = 5000;
+
+  for (size_t i = 0; i < 5000; ++i) {
+    std::array<unsigned char, 257> buf;
+    SimpleRandomizer::getInstance()->getRandomBytes(buf.data(), buf.size());
+    set.emplace(std::begin(buf), std::end(buf));
+  }
+
+  CPPUNIT_ASSERT_EQUAL(n, set.size());
+}
+
+} // namespace aria2
