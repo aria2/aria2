99def35884706f88c82cbb6d568f40201ea59159
Rachel Powers <508861+Ryex@users.noreply.github.com>
2023-03-24 15:57:48 -0700

    finish up wslay configuration
    Signed-off-by: Rachel Powers <508861+Ryex@users.noreply.github.com>

--- cmake/FindSphinx.cmake
+++ cmake/FindSphinx.cmake
@@ -17,4 +17,27 @@ find_program(
     HINTS ${_PYTHON_PATHS})
 mark_as_advanced(SPHINX_EXECUTABLE)
 
-find_package_handle_standard_args(Sphinx DEFAULT_MSG SPHINX_EXECUTABLE)
+if(SPHINX_EXECUTABLE)
+
+    if(WIN32 AND PYTHON_EXECUTABLE)
+        execute_process(COMMAND ${PYTHON_EXECUTABLE} ${SPHINX_EXECUTABLE} --version OUTPUT_VARIABLE SPHINX_VERSION_STR)
+    else()
+        execute_process(COMMAND ${SPHINX_EXECUTABLE} --version OUTPUT_VARIABLE SPHINX_VERSION_STR)
+    endif()
+
+    if (NOT "${SPHINX_VERSION_STR}" STREQUAL "")
+        if (SPHINX_VERSION_STR MATCHES "sphinx-build ([0-9]+\\.[0-9]+(\\.|a?|b?)([0-9]*)(b?)([0-9]*))")
+            set (SPHINX_VERSION "${CMAKE_MATCH_1}")
+        elseif (_Sphinx_VERSION MATCHES "Sphinx v([0-9]+\\.[0-9]+(\\.|b?)([0-9]*)(b?)([0-9]*))")
+            set (SPHINX_VERSION "${CMAKE_MATCH_1}")
+        elseif (_Sphinx_VERSION MATCHES "Sphinx \\(sphinx-build\\) ([0-9]+\\.[0-9]+(\\.|a?|b?)([0-9]*)(b?)([0-9]*))")
+            set (SPHINX_VERSION "${CMAKE_MATCH_1}")
+        endif ()
+    endif()
+endif()
+
+find_package_handle_standard_args(
+    Sphinx 
+    REQUIRED_VARS SPHINX_EXECUTABLE SPHINX_VERSION
+    VERSION_VAR SPHINX_VERSION
+)
