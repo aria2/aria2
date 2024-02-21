3fc5e20a6b84ff24b7cb95609cb6f806635258c0
guangfeng <gf@gfzj.us>
2024-02-21 21:13:48 +0800

    change PREF_MAX_CONNECTION_PER_SERVER from 16 to 2048

--- src/OptionHandlerFactory.cc
+++ src/OptionHandlerFactory.cc
@@ -440,7 +440,7 @@ std::vector<OptionHandler*> OptionHandlerFactory::createOptionHandlers()
   {
     OptionHandler* op(new NumberOptionHandler(PREF_MAX_CONNECTION_PER_SERVER,
                                               TEXT_MAX_CONNECTION_PER_SERVER,
-                                              "1", 1, 256, 'x'));
+                                              "1", 1, 2048, 'x'));
     op->addTag(TAG_BASIC);
     op->addTag(TAG_FTP);
     op->addTag(TAG_HTTP);
